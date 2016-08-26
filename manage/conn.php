7e827fbd00aac08c5af37e7a7e32db15032c4f8e
wangdong <woden0415@163.com>
2016-08-26 19:59:44 +0800

    mysqli方法连接数据库
    同上

--- manage/conn.php
+++ manage/conn.php
@@ -6,12 +6,10 @@
 	$sqlname = "root";
 	$sqlpwd = "root";
 
-
 	/*创建连接*/
-	$conn = new mysqli($servername, $username, $password);
+	$conn = mysqli_connect($servername, $username, $password)
 	/*检测连接*/
-	if ($conn->connect_error) {
-	    die("连接失败: " . $conn->connect_error);
-	} 
-		echo "连接成功";
+	if ($conn) {
+	    die("连接失败: " . mysqli_connect_error());
+	}
  ?>
