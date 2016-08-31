189c5b74f8fddd277b3e2874aa4bdb8ec9818052
鬼谷中妖 <562550212@qq.com>
2016-08-31 11:14:12 +0800

    20160831 woden公司
    实现删除后台功能

--- manage/listUsers.php
+++ manage/listUsers.php
@@ -1,5 +1,17 @@
 <?php 
 
-	
+	echo "显示所有用户";
+	include 'conn.php';
+	$sql = "SELECT * FROM user";
+	$check_query = mysqli_query($conn,$sql);
+	while ($rows = mysqli_fetch_array($check_query)) {
+		echo $rows["uid"],'<br>';
+		echo $rows["uname"],'<br>';
+		echo $rows["uemail"],'<br>';
+		echo $rows["upwd"],'<br>';		
+		echo $rows["regdate"],'<br>';
+		echo '<a href="delete.php?action=delete&uname='.$rows["uname"].'"><button>删除</button></a>';
+		echo "<br><br><br><br><br>";
+	}
 
  ?>
