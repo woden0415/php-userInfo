9d06459c04490cbdc9dcb24ea3a404fbb4eed59b
鬼谷中妖 <562550212@qq.com>
2016-08-31 09:32:48 +0800

    20160831 woden公司
    增加删除功能，删除之前采用bootstrap框架的两个新页面，增加显示列表页面（功能未实现）。

--- manage/delete.php
+++ manage/delete.php
@@ -3,8 +3,10 @@
 	/*edit by woden*/
 	if($_GET['action'] == "delete"){
 		$uname = $_GET["uname"];
-		echo $uname;
 	}
-
-
+	include 'conn.php';
+	$sql = "DELETE FROM user WHERE uname = '$uname'";
+	$user_query = mysqli_query($conn,$sql);
+	header("Location: ../tpl/login.html");
+	
  ?>
